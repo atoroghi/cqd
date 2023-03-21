9318c7b1e25962ee6eb56652eb85c4adae753605
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2023-03-21 14:43:44 -0400

    Update out.py

--- kbc/out.py
+++ kbc/out.py
@@ -22,13 +22,13 @@ if __name__ == '__main__':
                         elif "ComplEx" in line:
                             model_name = "ComplEx"
                     if "dataset" in line:
-                        if "FB15k" in line:
-                            dataset_name = "FB15k"
+                        if "FB15k-237" in line:
+                            dataset_name = "FB15k-237"
                         elif "NELL" in line:
                             dataset_name = "NELL"
                     if "rank" in line:
                         rank = re.search(r'\d+', line).group()
-                    if "reg" in line:
+                    if "reg" in line and "zer" not in line:
                         reg = line
                     if "batch_size" in line:
                         batch_size = line
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
                         if match:
                             hit3 = float(match.group(2))
-                            if dataset_name == 'FB15k':
+                            if dataset_name == 'FB15k-237':
                                 if model_name == 'SimplE':
                                     if hit3 > best_simple_fb:
                                         best_simple_fb = hit3
@@ -46,6 +46,7 @@ if __name__ == '__main__':
                                         best_simple_reg_fb = reg
                                         best_simple_rank_fb = rank
                                         best_simple_match_fb = line
+                                        best_simple_name_fb = filename
                                 elif model_name == 'ComplEx':
                                     if hit3 > best_complex_fb:
                                         best_complex_fb = hit3
@@ -53,6 +54,7 @@ if __name__ == '__main__':
                                         best_complex_reg_fb = reg
                                         best_complex_rank_fb = rank
                                         best_complex_match_fb = line
+                                        best_complex_name_fb = filename
                         
                             elif dataset_name == 'NELL':
                                 if model_name == 'SimplE':
@@ -62,6 +64,7 @@ if __name__ == '__main__':
                                         best_simple_reg_nell = reg
                                         best_simple_rank_nell = rank
                                         best_simple_match_nell = line
+                                        best_simple_name_nell = filename
                                 elif model_name == 'ComplEx':
                                     if hit3 > best_complex_nell:
                                         best_complex_nell = hit3
@@ -69,6 +72,7 @@ if __name__ == '__main__':
                                         best_complex_reg_nell = reg
                                         best_complex_rank_nell = rank
                                         best_complex_match_nell = line
+                                        best_complex_name_nell = filename
 
 
     print("Best SimplE FB15k: ", best_simple_fb)
@@ -76,18 +80,22 @@ if __name__ == '__main__':
     print("Best SimplE FB15k reg: ", best_simple_reg_fb)
     print("Best SimplE FB15k rank: ", best_simple_rank_fb)
     print("Best SimplE FB15k results: ", best_simple_match_fb)
+    print("Best SimplE Name FB15k: ", best_simple_name_fb)
     print("Best SimplE NELL: ", best_simple_nell)
     print("Best SimplE NELL batch size: ", best_simple_batch_size_nell)
     print("Best SimplE NELL reg: ", best_simple_reg_nell)
     print("Best SimplE NELL rank: ", best_simple_rank_nell)
     print("Best SimplE NELL results: ", best_simple_match_nell)
+    print("Best SimplE Name NELL: ", best_simple_name_nell)
     print("Best ComplEx FB15k: ", best_complex_fb)
     print("Best ComplEx FB15k batch size: ", best_complex_batch_size_fb)
     print("Best ComplEx FB15k reg: ", best_complex_reg_fb)
     print("Best ComplEx FB15k rank: ", best_complex_rank_fb)
     print("Best ComplEx FB15k results: ", best_complex_match_fb)
+    print("Best ComplEx Name FB15k: ", best_complex_name_fb)
     print("Best ComplEx NELL: ", best_complex_nell)
     print("Best ComplEx NELL batch size: ", best_complex_batch_size_nell)
     print("Best ComplEx NELL reg: ", best_complex_reg_nell)
     print("Best ComplEx NELL rank: ", best_complex_rank_nell)
     print("Best ComplEx NELL results: ", best_complex_match_nell)
+    print("Best ComplEx Name NELL: ", best_complex_name_nell)
