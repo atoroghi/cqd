19d432659e50a7e8c538d888f91d5dc941fbcb79
Armin Toroghi <49584324+atoroghi@users.noreply.github.com>
2023-06-05 16:53:37 -0400

    LFM

--- kbc/CC_simulator.py
+++ kbc/CC_simulator.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
 
     # Open a file for writing
 
-    cov_anchors = [1e-5,1e-4,1e-3,1e-2, 1e-1,1e1,1e2,1e3,1e4,1e5]
+    #cov_anchors = [1e-5,1e-4,1e-3,1e-2, 1e-1,1e1,1e2,1e3,1e4,1e5]
+    cov_anchors = [1e5]
     cov_vars = [1e-5,1e-4,1e-3,1e-2, 1e-1,1e1,1e2,1e3,1e4,1e5]
     cov_targets = [1e-5,1e-4,1e-3,1e-2, 1e-1,1e1,1e2,1e3,1e4,1e5]
 
@@ -17,8 +18,10 @@ if __name__ == '__main__':
     for cov_anchor in cov_anchors:
         for cov_var in cov_vars:
             for cov_target in cov_targets:
-                output_file = f"slurm_{cov_target}_{cov_anchor}.out"
-                command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/Movielens", "--model_path", "models/Movielens-SimplE-model-rank-50-epoch-40-1684629098.pt", "--dataset", "Movielens", "--candidates", "3", "--quantifier", "marginal_ui", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
+                output_file = f"slurm_{cov_anchor}_{cov_var}_{cov_target}.out"
+                #command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/Movielens", "--model_path", "models/Movielens-SimplE-model-rank-50-epoch-40-1684629098.pt", "--dataset", "Movielens", "--candidates", "3", "--quantifier", "marginal_ui", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
+                command = ["python3", "-m", "kbc.cqd_beam_bpl", "data/Movielens", "--model_path", "models/Movielens-SimplE-model-rank-50-epoch-40-1684629098.pt", "--dataset", "Movielens", "--candidates", "10", "--quantifier", "marginal_i", "--cov_target", str(cov_target), "--cov_var", str(cov_var), "--cov_anchor", str(cov_anchor)]
+
                 with open(output_file, "w") as f:
                     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     stdout, stderr = process.communicate()
@@ -26,4 +29,4 @@ if __name__ == '__main__':
                     if stderr:
                         f.write(stderr.decode())
                 return_code = process.returncode
-                print(f"Command returned with code {return_code} for cov_target={cov_target} and cov_anchor={cov_anchor}")
+                print(f"Command returned with code {return_code} for cov_target={cov_target}, cov_var={cov_var}, and cov_anchor={cov_anchor}")
--- kbc/out_beam.py
+++ kbc/out_beam.py
@@ -1,6 +1,47 @@
 import os, sys, re
 import numpy as np
 from prettytable import PrettyTable
+import seaborn as sns
+import matplotlib.pyplot as plt
+def plot_sanity_check(sorted_dict, k):
+    sns.set_theme(style="darkgrid")
+    ratios = []
+    values = []
+    for i in range(len(sorted_dict['cov_anchor'])):
+        ratio_float = sorted_dict['cov_target'][i] / sorted_dict['cov_anchor'][i]
+        ratio = int(np.log10(ratio_float))
+        if ratio in ratios:
+            continue
+        
+        ratios.append(ratio)
+        value = [sorted_dict[f'hits{k}_{j}'][i] for j in range(6)]
+        values.append(value)
+    num_colors = len(values)
+    # sort ratios
+
+    range_ratios = np.arange(-(num_colors)//2+1, (num_colors)//2 + 1)
+    sorted_ratios, sorted_values = [], []
+    for i in range_ratios:
+        sorted_ratios.append(i)
+        ind = np.where(np.array(ratios) == i)[0][0]
+        sorted_values.append(values[ind])
+    sorted_ratios.reverse(); sorted_values.reverse()
+    sorted_ratios = sorted_ratios[7:14]
+    sorted_values = sorted_values[7:14]
+
+    color_palette = sns.color_palette("tab10", n_colors=num_colors)
+    sorted_legends = [f'Ratio: 1e{ratio}' for ratio in sorted_ratios]
+    #sorted_legends.sort(key=lambda x: float(x.split(':')[1]), reverse=True)
+    fig, ax = plt.subplots()
+    for i, value in enumerate(sorted_values):
+        sns.lineplot(x=range(6), y=value, marker='o', dashes=True, label=sorted_legends[i], color=color_palette[i])
+    ax.legend(loc='center right')
+    #ax.set_xlim(-1, 6) 
+    ax.set_xlabel("Step", fontdict={'fontsize': 18})
+    ax.set_ylabel(f"Hits@{k}", fontdict={'fontsize': 18})
+    
+    plt.savefig(f'sanity_hits_{k}.pdf', format='pdf', dpi=1200)
+    plt.show()
 
 if __name__ == '__main__':
     directory = os.getcwd()
@@ -9,13 +50,14 @@ if __name__ == '__main__':
         Iterates through all files with ".out" extension in the given directory and looks for the result with
         the greatest hits@10.
         """
+    k = 10
     results = {'existential':{}, 'marginal UI':{}, 'instantiated':{}}
     for key in list(results.keys()):
         results[key]['cov_anchor'] = []
         results[key]['cov_var'] = []
         results[key]['cov_target'] = []
-        results[key]['hits10_0'] = []; results[key]['hits10_1'] = []; results[key]['hits10_3'] = []
-        results[key]['hits10_4'] = []; results[key]['hits10_5'] = []; results[key]['hits10_6'] = []
+        results[key][f'hits{k}_0'] = []; results[key][f'hits{k}_1'] = []; results[key][f'hits{k}_3'] = []
+        results[key][f'hits{k}_4'] = []; results[key][f'hits{k}_5'] = []; results[key][f'hits{k}_6'] = []
 
     hitsone_existential_all = np.array([]); hitsone_marginal_all = np.array([]); hitsone_instantiated_all = np.array([])
     hitsthree_existential_all = np.array([]); hitsthree_marginal_all = np.array([]); hitsthree_instantiated_all = np.array([])
@@ -54,19 +96,24 @@ if __name__ == '__main__':
                                 hitsone_existential = np.array(eval(matches1[0]))
                                 hitsthree_existential = np.array(eval(matches1[1]))
                                 hitsten_existential = np.array(eval(matches1[2]))
-                                results['existential']['hits10_0'].append(hitsten_existential[0])
-                                results['existential']['hits10_1'].append(hitsten_existential[1])
-                                results['existential']['hits10_3'].append(hitsten_existential[2])
-                                results['existential']['hits10_4'].append(hitsten_existential[3])
-                                results['existential']['hits10_5'].append(hitsten_existential[4])
-                                results['existential']['hits10_6'].append(hitsten_existential[5])
+                                if k == 1:
+                                    hits = hitsone_existential
+                                elif k == 3:
+                                    hits = hitsthree_existential
+                                elif k == 10:
+                                    hits = hitsten_existential
+                                results['existential'][f'hits{k}_0'].append(hits[0])
+                                results['existential'][f'hits{k}_1'].append(hits[1])
+                                results['existential'][f'hits{k}_3'].append(hits[2])
+                                results['existential'][f'hits{k}_4'].append(hits[3])
+                                results['existential'][f'hits{k}_5'].append(hits[4])
+                                results['existential'][f'hits{k}_6'].append(hits[5])
 
-                        elif "marginal" in line:
+                        elif "marginal_ui" in line or "marginal ui" in line:
+
                             if "cov_anchor" in line:
-                                print(line)
 
                                 matches = re.findall(pattern2, line)
-                                print(matches)
                                 cov_anchor = float(matches[0])
                                 results['marginal UI']['cov_anchor'].append(cov_anchor)
                             elif "cov_var" in line:
@@ -82,13 +129,20 @@ if __name__ == '__main__':
                                 hitsone_marginal = np.array(eval(matches1[0]))
                                 hitsthree_marginal = np.array(eval(matches1[1]))
                                 hitsten_marginal = np.array(eval(matches1[2]))
-                                results['marginal UI']['hits10_0'].append(hitsten_marginal[0])
-                                results['marginal UI']['hits10_1'].append(hitsten_marginal[1])
-                                results['marginal UI']['hits10_3'].append(hitsten_marginal[2])
-                                results['marginal UI']['hits10_4'].append(hitsten_marginal[3])
-                                results['marginal UI']['hits10_5'].append(hitsten_marginal[4])
-                                results['marginal UI']['hits10_6'].append(hitsten_marginal[5])
-                        elif "instantiated" in line:
+                                if k == 1:
+                                    hits = hitsone_marginal
+                                elif k == 3:
+                                    hits = hitsthree_marginal
+                                elif k == 10:
+                                    hits = hitsten_marginal
+                                results['marginal UI'][f'hits{k}_0'].append(hits[0])
+                                results['marginal UI'][f'hits{k}_1'].append(hits[1])
+                                results['marginal UI'][f'hits{k}_3'].append(hits[2])
+                                results['marginal UI'][f'hits{k}_4'].append(hits[3])
+                                results['marginal UI'][f'hits{k}_5'].append(hits[4])
+                                results['marginal UI'][f'hits{k}_6'].append(hits[5])
+
+                        elif "instantiated" in line or 'marginal_i' in line:
                             if "cov_anchor" in line:
                                 matches = re.findall(pattern2, line)
                                 cov_anchor = float(matches[0])
@@ -106,40 +160,46 @@ if __name__ == '__main__':
                                 hitsone_marginal = np.array(eval(matches1[0]))
                                 hitsthree_marginal = np.array(eval(matches1[1]))
                                 hitsten_marginal = np.array(eval(matches1[2]))
-                                results['instantiated']['hits10_0'].append(hitsten_marginal[0])
-                                results['instantiated']['hits10_1'].append(hitsten_marginal[1])
-                                results['instantiated']['hits10_3'].append(hitsten_marginal[2])
-                                results['instantiated']['hits10_4'].append(hitsten_marginal[3])
-                                results['instantiated']['hits10_5'].append(hitsten_marginal[4])
-                                results['instantiated']['hits10_6'].append(hitsten_marginal[5])
+                                if k == 1:
+                                    hits = hitsone_marginal
+                                elif k == 3:
+                                    hits = hitsthree_marginal
+                                elif k == 10:
+                                    hits = hitsten_marginal
+                                results['instantiated'][f'hits{k}_0'].append(hits[0])
+                                results['instantiated'][f'hits{k}_1'].append(hits[1])
+                                results['instantiated'][f'hits{k}_3'].append(hits[2])
+                                results['instantiated'][f'hits{k}_4'].append(hits[3])
+                                results['instantiated'][f'hits{k}_5'].append(hits[4])
+                                results['instantiated'][f'hits{k}_6'].append(hits[5])
+
 
     for quantifier in ['existential', 'marginal UI', 'instantiated']:
-        print(quantifier)
         quantifier_results = results[quantifier]
+        
         quantifier_results_sorted = sorted(zip(quantifier_results['cov_anchor'], quantifier_results['cov_var'], quantifier_results['cov_target'],
-                                                  quantifier_results['hits10_0'], quantifier_results['hits10_1'], quantifier_results['hits10_3'],
-                                                    quantifier_results['hits10_4'], quantifier_results['hits10_5'], quantifier_results['hits10_6']), key=lambda x: x[-1], reverse=True)
-
-
+                                                  quantifier_results[f'hits{k}_0'], quantifier_results[f'hits{k}_1'], quantifier_results[f'hits{k}_3'],
+                                                    quantifier_results[f'hits{k}_4'], quantifier_results[f'hits{k}_5'], quantifier_results[f'hits{k}_6']), key=lambda x: x[-1], reverse=True)
 
         sorted_dict = {
             'cov_anchor': [x[0] for x in quantifier_results_sorted],
             'cov_var': [x[1] for x in quantifier_results_sorted],
             'cov_target': [x[2] for x in quantifier_results_sorted],
-            'hits10_0': [x[3] for x in quantifier_results_sorted],
-            'hits10_1': [x[4] for x in quantifier_results_sorted],
-            'hits10_3': [x[5] for x in quantifier_results_sorted],
-            'hits10_4': [x[6] for x in quantifier_results_sorted],
-            'hits10_5': [x[7] for x in quantifier_results_sorted],
-            'hits10_6': [x[8] for x in quantifier_results_sorted]
-
+            f'hits{k}_0': [x[3] for x in quantifier_results_sorted],
+            f'hits{k}_1': [x[4] for x in quantifier_results_sorted],
+            f'hits{k}_2': [x[5] for x in quantifier_results_sorted],
+            f'hits{k}_3': [x[6] for x in quantifier_results_sorted],
+            f'hits{k}_4': [x[7] for x in quantifier_results_sorted],
+            f'hits{k}_5': [x[8] for x in quantifier_results_sorted]
         }
-
-        table = PrettyTable(['cov_anchor', 'cov_var', 'cov_target', 'hits10_0', 'hits10_1', 'hits10_3', 'hits10_4', 'hits10_5', 'hits10_6'])
+        table = PrettyTable(['cov_anchor', 'cov_var', 'cov_target', f'hits{k}_0', f'hits{k}_1', f'hits{k}_3', f'hits{k}_4', f'hits{k}_5', f'hits{k}_6'])
 
         for i in range(len(sorted_dict['cov_anchor'])):
-            table.add_row([sorted_dict['cov_anchor'][i], sorted_dict['cov_var'][i], sorted_dict['cov_target'][i], sorted_dict['hits10_0'][i], sorted_dict['hits10_1'][i],
-                           sorted_dict['hits10_3'][i], sorted_dict['hits10_4'][i], sorted_dict['hits10_5'][i], sorted_dict['hits10_6'][i]])
+            table.add_row([sorted_dict['cov_anchor'][i], sorted_dict['cov_var'][i], sorted_dict['cov_target'][i], sorted_dict[f'hits{k}_0'][i], sorted_dict[f'hits{k}_1'][i],
+                           sorted_dict[f'hits{k}_2'][i], sorted_dict[f'hits{k}_3'][i], sorted_dict[f'hits{k}_4'][i], sorted_dict[f'hits{k}_5'][i]])
 
         print(table)
-
+        #if quantifier == 'marginal UI':
+        #    plot_sanity_check(sorted_dict, k)
+    
+        
